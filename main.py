33c72fd355a9e1a2606012c5ad8b8aa2d9e979a5
mrkcc18 <mrk.cc2@gmail.com>
2025-05-22 10:12:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import os
-from telegram.ext import ApplicationBuilder
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! بات روی Webhook اجرا شده ✅")
+    await update.message.reply_text("سلام! ربات روی Webhook اجرا شده و فعاله ✅")
 
 def main():
     TOKEN = os.getenv("TELEGRAM_TOKEN")
-    WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # آدرس عمومی سرویست در Render
+    WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # آدرس عمومی پروژه‌ات در Render
 
     app = ApplicationBuilder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
 
+    # راه‌اندازی Webhook
     app.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get("PORT", 10000)),
@@ -21,4 +26,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
