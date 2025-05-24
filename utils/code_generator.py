ee0fb1c7bac54dcfb825496fb23e062048496654
mrkcc18 <mrk.cc2@gmail.com>
2025-05-24 03:39:05 +0330

    Update code_generator.py

--- utils/code_generator.py
+++ utils/code_generator.py
@@ -1,9 +1,4 @@
 import random
-from datetime import datetime
 
 def generate_user_code(name: str) -> str:
-    now = datetime.now()
-    initials = ''.join([word[0] for word in name.split()[:2]]).upper()
-    factor = str(random.randint(1, 9))
-    code = f"{initials}{factor}{now.strftime('%y%m%d%H')}"
-    return code
+    return str(random.randint(100000, 999999))
